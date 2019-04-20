491aa8cb99e99b567bbeefc921a42872c7f29f6a
Lee Jinseop <js428118@gmail.com>
2019-04-20 19:53:18 +0900

    포트폴리오 프로젝트 링크 연결

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,25 @@
+const moveProject = function(id) {
+  switch (id) {
+    case 'wordHunter':
+      window.open('https://github.com/jinseoplee/word-hunter', '_blank');
+      break;
+    case 'portfolio':
+      window.open('https://github.com/jinseoplee/portfolio', '_blank');
+      break;
+    case 'webGame':
+      window.open('https://jinseoplee.github.io/web-game/', '_blank');
+      break;
+    default:
+  }
+};
+
 window.onload = () => {
+
+  const projects = document.querySelectorAll('.project');
+
+  for (const project of projects) {
+    project.addEventListener('click', (e) => {
+      moveProject(project.id);
+    });
+  }
 };
